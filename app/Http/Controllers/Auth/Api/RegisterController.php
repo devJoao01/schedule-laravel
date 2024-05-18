c8badeca462c8e60add9b4eabff7e060baa689b4
João Vitor <jvlima0002020@gmail.com>
2024-05-18 01:08:35 -0300

    create new registerController and create reuquest to validate filds

--- app/Http/Controllers/Auth/Api/RegisterController.php
+++ app/Http/Controllers/Auth/Api/RegisterController.php
@@ -2,10 +2,44 @@
 
 namespace App\Http\Controllers\Auth\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegistrationRequest;
 
 class RegisterController extends Controller
 {
-    //
+    public function register(RegistrationRequest $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'error when creating user'
+            ], 201);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 201);
+    }
 }
